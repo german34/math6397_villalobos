f409630d8fc444ea17e8f45b38cb611a46934293
German Villalobos <german.villalobos0@gmail.com>
2022-08-30 00:24:08 -0500

    Adding exercises 1 and 2 from first homework. Implemented as individual scripts. Can be run via terminal by running python filname.py.

---
+++ exercise3.py
@@ -0,0 +1,14 @@
+'''
+Exercise 1.3
+'''
+
+#Load the Yale B image data base and compute the economy SVD using a standard svd command
+
+#Compute the SVD with the method of snapshots
+
+#Compare the singular value spectra on a log plot.
+#Compare the first 10 left singular vectors using each method
+#(remember to reshape them into shape of a face).
+
+#Now compare a few singular vectors farther down the spectrum.
+#Explain your findings
---
+++ exercise4.py
@@ -0,0 +1,10 @@
+'''
+Exercise 1.4
+'''
+
+#Generate a random 100 by 100 matrix, whose entries are samples from a normal distribution
+#Plot the distribution of singular values in a box-and-whisker plot.
+#Plot the mean and median singular values as a function of r.
+
+#Now repeat this for different matrix sizes
+#Ex: 50by50, 200by200, 500by500, 1000by1000
